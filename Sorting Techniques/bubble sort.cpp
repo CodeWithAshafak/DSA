1c53287a16d24a13fbc1dbe74d248db3a9c605a5
CodeWithAshafak <143120421+Ashafak-7@users.noreply.github.com>
2024-10-15 22:28:25 +0530

    BRUTEFORCE

---
+++ Sorting Techniques/bubble sort.cpp
@@ -0,0 +1,38 @@
+ #include <iostream>
+using namespace std;
+int main()
+{
+    int arr[] ={1,2,3,5,4};
+    int tmp=0;
+    int count=0;
+    
+    int s = sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<s;i++)
+    {
+    	cout<<arr[i]<<"\t";
+	}
+	cout<<"After sortig of the array the result is \n";
+	for(int i=0;i<s;i++)
+	{
+		for(int j=0;j<s-1;j++)
+		{
+			if(arr[j]>arr[j+1])
+			{
+				tmp = arr[j+1];
+				arr[j+1]=arr[j];
+				arr[j]=tmp;
+				count++;
+			}
+		}
+	}
+	for(int i=0;i<s;i++)
+    {
+    	cout<<arr[i]<<"\t";
+	}
+	
+	cout<<"\n"<<"count is "<<count;
+ 	return 0;
+}
+
+
+	
