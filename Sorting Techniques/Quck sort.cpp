5696fe17c510214b14130fb717c4454e5bd5ded8
CodeWithAshafak <ashfak04@gmail.com>
2024-12-02 11:41:06 +0530

    multiple code

--- Sorting Techniques/Quck sort.cpp
+++ Sorting Techniques/Quck sort.cpp
@@ -1,4 +1,4 @@
-//Quick sort :-
+o//Quick sort :-
 /*
 used for large dataset 
 => in this method array devide into two parts 
@@ -10,7 +10,7 @@ used for large dataset
 =>1> consider the first element of array is the pivot element , consider the two poiner P,Q. P pointer pivot element ke ekdam next vala element pr laga hai
 and pointer Q last me lagana hai.
 =>2> if P>=pivot element then stop also if Q<=pivot element then stop.
-=>3> if (P>Q) swap(P,Q) [if P,Q is not crossed to the array ]
+=>3> if (P>Q) swap(P,Q) [ if P,Q is not crossed to the array ]
 =>4> if(p cross Q) swap(Q, pivot) [ if P,Q is crossed to each other]
  
 => it is called divide and conquire method.
@@ -18,14 +18,44 @@ and pointer Q last me lagana hai.
 
 #include<iostream>
 using namespace std;
-// main QS funciton 
+//
+int srt(int arr[],int low,int high)
+{
+	int pvt=arr[low]; // setting arr[0] as the pivot element 
+	int p =low+1;     // setting p next to pivit element 
+	int q =high;      // setting q to the high
+	int tmp;
+	do
+	{
+		while(arr[p]<pvt)
+		{
+			p++;
+		}
+		while(arr[q]>pvt)
+		{
+			q--;
+		}
+		if(p<q)// if p is not crossed the q
+		{
+			tmp = arr[p];
+			arr[p]=arr[q];
+			arr[q]=tmp;
+		}
+		
+	}while(p<q); // jaise hi while wali condition fail hogi i.e. p q ko corss kar diyaq
+	tmp=arr[low];
+	arr[low]=arr[q];
+	arr[q]=tmp;
+	return q;
+}
+// main QS  funciton 
 void qs(int  arr[],int low,int high)
 {
     if(low<high)
         {
             int pvt;
             pvt=srt(arr,low,high);
-            qs(arr,0,pvt-1);//step-2 of QS i.e. Recursive Relationship
+            qs(arr,low,pvt-1);//step-2 of QS i.e. Recursive Relationship
             qs(arr,pvt+1,high);//step-2 of QS i.e. Recursive Relationship
         }
 }
@@ -34,7 +64,7 @@ void prntarr(int arr[],int s)
 {
     for(int i=0;i<s;i++)
         {
-            cout<<arr[i]<<"\n";
+            cout<<arr[i]<<"\t ";
         }
 }
 
@@ -42,7 +72,9 @@ void prntarr(int arr[],int s)
 int main()
 {
 
-int arr[]={3,1,5,2,6};
+int arr[]={35,50,15,25,80};
 prntarr(arr,5);
 qs(arr,0,4);
+cout<<"\n after quick sort\n";
+prntarr(arr,5);
 }
--- Sorting Techniques/insertion sort updated.cpp
+++ Sorting Techniques/insertion sort updated.cpp
@@ -1,36 +1,74 @@
 //insertion sort
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//int arr[]={12,23,31,40,15,60};
+//int sorted,tmp;
+//int n=sizeof(arr)/sizeof(arr[0]);
+//int count=0;
+//
+//
+//for(int i=0;i<n;i++)
+//{
+//   cout<<arr[i]<<"\t";
+//}
+//cout<<"\n";
+//for(int i=1;i<n;i++)
+//{
+//    sorted=i;
+//    while(sorted>0 && arr[sorted]<arr[sorted-1])
+//    {
+//        tmp=arr[sorted-1];//1
+//        arr[sorted-1]=arr[sorted];//2
+//        arr[sorted]=tmp;
+//        sorted--;
+//        count++;
+//    }
+//}
+//    cout<<"\n after insertion sort\n";
+//    for(int i=0;i<n;i++)
+//        {
+//            cout<<arr[i]<<"\t";
+//        }
+//cout<<"\n";
+//cout<<"count is "<<count;
+//}
+
+
+//insertion sort:-
+
 #include<iostream>
 using namespace std;
-int main()
-{
-int arr[]={1,2,3,4,5,6};
-int sorted,tmp;
-int n=sizeof(arr)/sizeof(arr[0]);
-int count=0;
-
-
-for(int i=0;i<n;i++)
-{
-cout<<arr[i]<<"\t";
-}
-cout<<"\n";
-for(int i=1;i<n;i++)
-{
-sorted=i;
-while(sorted>0 && arr[sorted]<arr[sorted-1])
-{
-tmp=arr[sorted-1];//1
-arr[sorted-1]=arr[sorted];//2
-arr[sorted]=tmp;
-sorted--;
-count++;
-}
-}
-cout<<"\n after insertion sort\n";
-for(int i=0;i<n;i++)
-{
-cout<<arr[i]<<"\t";
-}
-
-cout<<"count is "<<count;
+int main(){
+	
+	int arr[]={43,6,57,23,90,54,23,67,12};
+	int s = sizeof(arr)/sizeof(arr[0]);
+	int sorted , temp;
+	for(int i=0;i<s ;i++)
+	{
+		cout<<arr[i]<<"\t";
+	}
+	cout<<"\n";
+	cout<<"after appling the bubble sort :- \n";
+	
+	
+	for(int i=1;i<s;i++)
+	{
+		sorted = i;
+	     while(sorted>0 && arr[sorted]<arr[sorted-1])
+		{
+			temp= arr[sorted-1];
+			arr[sorted-1]=arr[sorted];
+			arr[sorted]=temp;
+			 sorted--;
+			
+		}
+	}
+	for(int i=0;i<s;i++)
+	{
+		cout<<arr[i]<<"\t";
+	}
+	
+	return 0;
 }
